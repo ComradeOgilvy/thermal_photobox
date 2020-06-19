e483f162afba9ce229b34d18a01d3a973d881dea
Steve Starke <steve.starke@mailbox.tu-dresden.de>
2020-06-19 09:53:38 +0200

    minor changes to LED and linting

--- thermal_photobox.py
+++ thermal_photobox.py
@@ -18,6 +18,8 @@ import RPi.GPIO as GPIO
 """
 GPIO Initialization
 """
+
+
 def _initialize_GPIO(GPIO_config):
     logging.info("Initialize GPIO")
     GPIO.setwarnings(False)
@@ -29,9 +31,12 @@ def _initialize_GPIO(GPIO_config):
     # Arcade Button
     GPIO.setup(GPIO_config["button_pin"], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+
 """
 Camera Initialization
 """
+
+
 def _initialize_camera(config_camera):
     logging.info("Initialize Camera")
     # Parse configuration options
@@ -43,27 +48,29 @@ def _initialize_camera(config_camera):
     annotate_background = config_camera["annotate_background"]
 
     camera = PiCamera()
-    logging.debug("Image Resolution: Height=[%s]; Width=[%s]" %(resolution_height,resolution_width))
+    logging.debug("Image Resolution: Height=[%s]; Width=[%s]" % (
+        resolution_height, resolution_width))
     camera.resolution = (resolution_height, resolution_width)
     # turn camera to black and white
-    camera.color_effects = (128,128)
+    camera.color_effects = (128, 128)
     camera.contrast = config_camera["contrast"]
     camera.brightness = config_camera["brightness"]
     camera.exposure_mode = 'auto'
 
     if config_camera["annotate"]:
-        logging.debug("Set annotate text size to [%s]" %(annotate_text_size))
+        logging.debug("Set annotate text size to [%s]" % (annotate_text_size))
         camera.annotate_text_size = annotate_text_size
         camera.annotate_foreground = Color(annotate_foreground)
         camera.annotate_background = Color(annotate_background)
         text = ' ' + annotate_text + ' '
-        logging.debug("Annotate text is [%s]" %(text))
+        logging.debug("Annotate text is [%s]" % (text))
         camera.annotate_text = text
 
     logging.info("Start Camera Preview")
     camera.start_preview()
 
     return camera
+
 
 """
 Helper Function to call an bash command
@@ -75,17 +82,20 @@ Helper Function to call an bash command
   - print end of command
   - print output of command
 """
-def _launch_command(command:list):
+
+
+def _launch_command(command: list):
     try:
-        logging.debug('Start subprocess '+'"'+ ' '.join(map(str,command))+'"')
+        logging.debug('Start subprocess '+'"' +
+                      ' '.join(map(str, command))+'"')
         process = subprocess.Popen(
-            command, 
+            command,
             stdout=subprocess.PIPE,
             stdin=subprocess.DEVNULL,
             stderr=subprocess.STDOUT,
         )
-        logging.debug('Finished subprocess '+'"'+ 
-            ' '.join(map(str,command))+'"')
+        logging.debug('Finished subprocess '+'"' +
+                      ' '.join(map(str, command))+'"')
         output = process.stdout.read().decode('UTF-8').rstrip()
         logging.debug('[output] %s', output)
         return output
@@ -96,10 +106,11 @@ def _launch_command(command:list):
         return None
     except subprocess.CalledProcessError as e:
         logging.exception(
-            "The subprocess failed with: output = %s, error code = %s" 
-            %(e.output, e.returncode)
+            "The subprocess failed with: output = %s, error code = %s"
+            % (e.output, e.returncode)
         )
         return None
+
 
 def _print_image(image_path):
     print_image_command = [
@@ -114,6 +125,7 @@ def _print_image(image_path):
     _launch_command(print_image_command)
 
     return
+
 
 def _wait_for_printer():
     logging.info("Wait for printer to finish printing")
@@ -136,7 +148,7 @@ def _take_image(config_output, config_camera, camera):
         config_output["image_counter"] += 1
         update_image_counter(config_output["image_counter"])
         logging.debug("Image counter is [%s]", config_output["image_counter"])
-    
+
     output_path = config_output["output_path"]
     image_name = config_output["image_name"]
     image_counter = config_output["image_counter"]
@@ -145,10 +157,11 @@ def _take_image(config_output, config_camera, camera):
     config_output["current_image_path"] = image_path
     logging.debug("Image Path is [%s]", image_path)
 
-    logging.info("Capture Picture, Image_path is [%s]" %image_path)
+    logging.info("Capture Picture, Image_path is [%s]" % image_path)
     camera.capture(image_path)
 
     return config_output
+
 
 def _red_led_blinking(red_led_pin):
     logging.info("Red LED blinking")
@@ -158,17 +171,20 @@ def _red_led_blinking(red_led_pin):
         time.sleep(0.5)
         GPIO.output(red_led_pin, GPIO.HIGH)
 
+
 def _red_led_flashing(red_led_pin):
     logging.info("Red LED flashing")
-    for x in range(2):
+    for x in range(3):
         time.sleep(0.2)
         GPIO.output(red_led_pin, GPIO.LOW)
         time.sleep(0.2)
         GPIO.output(red_led_pin, GPIO.HIGH)
 
+
 def update_image_counter(image_counter):
     with open('counter', 'w') as counter:    # save
-        counter.write(image_counter)
+        counter.write(str(image_counter))
+
 
 """
 Main Logic
@@ -183,6 +199,8 @@ Main Logic
     - deactivate red led
     - activate green led
 """
+
+
 def _main(config, camera):
     green_led_pin = config["GPIO"]["green_led_pin"]
     red_led_pin = config["GPIO"]["red_led_pin"]
@@ -199,11 +217,13 @@ def _main(config, camera):
             # Red LED blinking and flashing for visual feedback
             _red_led_blinking(red_led_pin)
             _red_led_flashing(red_led_pin)
-
-            # Start
-            config["output"] = _take_image(config["output"],config["camera"],camera)
+            
+            GPIO.output(red_led_pin, GPIO.LOW)
+            # Start Capturing the image
+            config["output"] = _take_image(
+                config["output"], config["camera"], camera)
             _print_image(config["output"]["current_image_path"])
-
+            GPIO.output(red_led_pin, GPIO.HIGH)
             _wait_for_printer()
 
             # Switch LEDs back
@@ -213,13 +233,16 @@ def _main(config, camera):
         time.sleep(0.05)
     return 0
 
+
 """
 Main Initialization
 """
+
+
 def main(arguments):
     try:
         t0 = time.monotonic()
-        exit_status=2
+        exit_status = 2
         """
         Initialize configuration parser
         """
@@ -233,7 +256,7 @@ def main(arguments):
                 "Cannot open Configuration File: [%s]", str(e)
             )
             raise
-        
+
         """ 
         Initialize the logger
         """
@@ -248,7 +271,7 @@ def main(arguments):
             logging.error(
                 "No write access to %s, put log file into current working directory",
                 log_file
-                )
+            )
             log_file = os.getcwd()+'/thermal_photobox.log'
         logging_handler = logging.FileHandler(
             log_file,
@@ -269,60 +292,80 @@ def main(arguments):
         config["camera"] = {}
         config["GPIO"] = {}
         try:
-            config["output"]["output_path"] = cfg.get("output","output_path")
+            config["output"]["output_path"] = cfg.get("output", "output_path")
             logging.info("Output-path: [%s]", config["output"]["output_path"])
-            config["output"]["temporary"] = cfg.getboolean("output", "temporary")
+            config["output"]["temporary"] = cfg.getboolean(
+                "output", "temporary")
             if config["output"]["temporary"]:
                 logging.info("Images are saved only temporary")
             else:
                 logging.info("Images are saved permanently")
-            config["output"]["image_name"] = cfg.get("output","image_name")
-            logging.info("Base image name is [%s]", config["output"]["image_name"])
+            config["output"]["image_name"] = cfg.get("output", "image_name")
+            logging.info(
+                "Base image name is [%s]", config["output"]["image_name"])
             config["camera"]["annotate"] = cfg.getboolean("camera", "annotate")
             if config["camera"]["annotate"]:
                 logging.info("Annotation is enabled")
             else:
                 logging.info("Annotation is disabled")
-            config["camera"]["annotate_text"] = cfg.get("camera", "annotate_text")
-            logging.info("Annotate text is [%s]", config["camera"]["annotate_text"])
-            config["camera"]["annotate_text_size"] = cfg.getint("camera","annotate_text_size")
-            logging.info("Annotate text size is [%s]", config["camera"]["annotate_text_size"])
-            config["camera"]["annotate_foreground"] = cfg.get("camera", "annotate_foreground")
-            logging.info("Annotate foreground color is [%s]", config["camera"]["annotate_foreground"])
-            config["camera"]["annotate_background"] = cfg.get("camera", "annotate_background")
-            logging.info("Annotate background color is [%s]", config["camera"]["annotate_background"])
-            config["camera"]["resolution_height"] = cfg.getint("camera", "resolution_height")
-            logging.info("Image resolution height is [%s]", config["camera"]["resolution_height"])
-            config["camera"]["resolution_width"] = cfg.getint("camera", "resolution_width")
-            logging.info("Image resolution width is [%s]", config["camera"]["resolution_width"])
+            config["camera"]["annotate_text"] = cfg.get(
+                "camera", "annotate_text")
+            logging.info("Annotate text is [%s]",
+                         config["camera"]["annotate_text"])
+            config["camera"]["annotate_text_size"] = cfg.getint(
+                "camera", "annotate_text_size")
+            logging.info(
+                "Annotate text size is [%s]", config["camera"]["annotate_text_size"])
+            config["camera"]["annotate_foreground"] = cfg.get(
+                "camera", "annotate_foreground")
+            logging.info(
+                "Annotate foreground color is [%s]", config["camera"]["annotate_foreground"])
+            config["camera"]["annotate_background"] = cfg.get(
+                "camera", "annotate_background")
+            logging.info(
+                "Annotate background color is [%s]", config["camera"]["annotate_background"])
+            config["camera"]["resolution_height"] = cfg.getint(
+                "camera", "resolution_height")
+            logging.info(
+                "Image resolution height is [%s]", config["camera"]["resolution_height"])
+            config["camera"]["resolution_width"] = cfg.getint(
+                "camera", "resolution_width")
+            logging.info(
+                "Image resolution width is [%s]", config["camera"]["resolution_width"])
             config["camera"]["contrast"] = cfg.getint("camera", "contrast")
-            logging.info("Camera contrast is [%s]", config["camera"]["contrast"])
+            logging.info(
+                "Camera contrast is [%s]", config["camera"]["contrast"])
             config["camera"]["brightness"] = cfg.getint("camera", "brightness")
-            logging.info("Camera brightness is [%s]", config["camera"]["brightness"])
-            config["GPIO"]["button_pin"] = cfg.getint("GPIO","button_pin")
+            logging.info(
+                "Camera brightness is [%s]", config["camera"]["brightness"])
+            config["GPIO"]["button_pin"] = cfg.getint("GPIO", "button_pin")
             logging.info("Button GPIO is [%s]", config["GPIO"]["button_pin"])
-            config["GPIO"]["green_led_pin"] = cfg.getint("GPIO","green_led_pin")
-            logging.info("Green LED GPIO is [%s]", config["GPIO"]["green_led_pin"])
-            config["GPIO"]["red_led_pin"] = cfg.getint("GPIO","red_led_pin")
+            config["GPIO"]["green_led_pin"] = cfg.getint(
+                "GPIO", "green_led_pin")
+            logging.info("Green LED GPIO is [%s]",
+                         config["GPIO"]["green_led_pin"])
+            config["GPIO"]["red_led_pin"] = cfg.getint("GPIO", "red_led_pin")
             logging.info("Red LED GPIO is [%s]", config["GPIO"]["red_led_pin"])
         except(configparser.NoSectionError, configparser.NoOptionError) as e:
-            logging.exception("Could not parse configuration options. Error: [%s]", str(e))
+            logging.exception(
+                "Could not parse configuration options. Error: [%s]", str(e))
             raise
 
         # image counter is used for the image file name(s)
         logging.info("Read image counter")
-        with open('counter','r') as counter:
+        with open('counter', 'r') as counter:
             config["output"]["image_counter"] = int(counter.read())
-            logging.info("Image counter is [%s]", config["output"]["image_counter"])
+            logging.info("Image counter is [%s]",
+                         config["output"]["image_counter"])
 
         """
         Start counting the time and start application
         """
         t0 = time.monotonic()
         logging.critical(
-                "=== START OF APPLICATION AT %s ===",
-                datetime.utcnow().isoformat()
-            )
+            "=== START OF APPLICATION AT %s ===",
+            datetime.utcnow().isoformat()
+        )
 
         try:
             # Initialize GPIOs
@@ -333,7 +376,7 @@ def main(arguments):
             """
             start main logic
             """
-            exit_status = _main(config,camera)
+            exit_status = _main(config, camera)
         except Exception as e:
             exit_status = 3
             logging.exception("Execution failed: %s", str(e))
@@ -341,8 +384,10 @@ def main(arguments):
 
     finally:
         # GPIO Cleanup
+        logging.info("GPIO Cleanup")
         GPIO.cleanup()
         # Camera Cleanup
+        logging.info("Camera Cleanup")
         camera.stop_preview()
         camera.close()
         # Calculate Duration and exit the application
@@ -355,6 +400,7 @@ def main(arguments):
         )
 
         sys.exit(exit_status)
+
 
 """
 Load the CLI parameters
